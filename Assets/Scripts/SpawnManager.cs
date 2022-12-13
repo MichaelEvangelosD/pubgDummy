a51f38f8768d9991deea1dfa4cf3e7fc08a2662e
VPinis03 <92648518+VPinis03@users.noreply.github.com>
2022-12-13 16:24:03 +0200

    RESPAWNER

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -93,6 +93,7 @@ public class SpawnManager : MonoBehaviourPunCallbacks
         while (true)
         {
             yield return new WaitForSeconds(waitTime);
+            Respawner();
         }
     }
 }
